9fd12ef32f4f3cb5c75ec0f410e68ff224b9fc8d
Juan Pablo Campos <64780248+Jp3222@users.noreply.github.com>
2021-03-25 04:22:07 -0600

    Create Graficacion_Unidad1_T1.java

---
+++ graficacion_unidad1_t1/Graficacion_Unidad1_T1.java
@@ -0,0 +1,12 @@
+package graficacion_unidad1_t1;
+
+import javax.swing.SwingUtilities;
+
+public class Graficacion_Unidad1_T1 {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()->new Ventana().setVisible(true));
+        
+    }
+    
+}
